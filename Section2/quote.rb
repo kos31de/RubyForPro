01ddc557be5cedc7832f3623ec2a287ee61e4648
[kousy] <[koseiide@gmail.com]>
2018-10-14 08:55:33 +0900

    \で式展開やクォート打ち消せる。P.25

---
+++ Section2/quote.rb
@@ -0,0 +1,13 @@
+puts "こんにちは\nさようなら"
+puts 'こんにちは\nさようなら'
+
+name = 'Alice'
+puts 'Hello, #{name}'
+
+#ダブルクォートを使う文字列でバックスラッシュで\nを打ち消す
+name = 'Alice'
+puts "Hello, \#{name}
+
+#シングルクォート文字列の中にシングルクォートを、ダブルクォート文字列の中にダブルクォートを入れたい時も\をつける"
+puts 'He said, "Don\'t speak."'
+puts "He said, \"Don't speak.\""
