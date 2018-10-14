62c5a43c990679958eb4b1960e337fb81667b8e2
[kousy] <[koseiide@gmail.com]>
2018-10-14 18:47:19 +0900

    ()使って分かりやすくするP.33

---
+++ Section2/logic.rb
@@ -0,0 +1,15 @@
+t1 = true
+t2 = true
+f1 = false
+
+puts t1 && t2
+puts t1 && f1
+puts t1 || f1
+f2 = false
+puts f1 || f2
+
+puts t1 && t2 || f1 && f2
+#分かりやすく書くと
+puts (t1 && t2) || (f1 && f2)
+
+
---
+++ Section2/logic3.rb
@@ -0,0 +1,4 @@
+t1 = true
+f1 = false
+puts !t1
+puts !f1
