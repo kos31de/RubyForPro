bd7e94c47c0f418a8bf33183a380f15ee697b59c
[kousy] <[koseiide@gmail.com]>
2018-10-16 13:42:32 +0900

    普通に<<-が良さそうP.44

--- ruby-book/lib/here_document.rb
+++ ruby-book/lib/here_document.rb
@@ -10,3 +10,18 @@ a = <<TEXT
   複数行に渡る長い文字列を作成するのに便利です。
 TEXT
 puts a
+
+<<HTML
+<div>
+  <img src="sample.jpg">
+</div>
+HTML
+
+def some_method
+  <<-TEXT
+これはヒアドキュメントです。
+<<-を使うと最後の識別子をインデントさせることができます。
+  TEXT
+end
+
+puts some_method
