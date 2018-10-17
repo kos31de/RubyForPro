76b85d888d3ff621d058c030e12e2438646f3c81
[kou-sy] <e153416@eve.u-ryukyu.ac.jp>
2018-10-17 18:35:26 +0900

    ?のメソッドは真偽値判定につかうよ

---
+++ ruby-book/lib/question_method.rb
@@ -0,0 +1,23 @@
+=begin
+a.empty?
+'abc'.empty?
+
+'watch'.include?('at')
+'watch'.include?('in')
+
+1.odd?
+2.odd?
+
+1.even?
+2.even?
+
+nil.nil?
+'abc'.nil?
+1.nil?
+=end
+def multiple_of_three?(n)
+  puts n % 3 == 0
+end
+multiple_of_three?(4)
+multiple_of_three?(5)
+multiple_of_three?(6)
