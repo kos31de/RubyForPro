9284d3ef48415b6d1bcc662e38036fba368876d4
[kou-sy] <e153416@eve.u-ryukyu.ac.jp>
2018-10-21 16:50:47 +0900

    以上未満みたいなのは範囲オブジェクトでシンプルに書けるNo.2826

--- Section4/range.rb
+++ Section4/range.rb
@@ -3,3 +3,23 @@ puts range.include?(5)
 
 range_2 = 1...5
 puts range_2.include?(5)
+
+a = [1, 2, 3, 4, 5]
+puts a[1..3]
+
+a = 'abcdef'
+puts a[1..3]
+
+def liquid?(temperature)
+  0 <= temperature && temperature < 100
+end
+
+puts liquid?(-1)
+puts liquid?(99)
+
+# 範囲オブジェクトだとシンプルに書ける
+def liquid?(temperature)
+  (0..100).include?(temperature)
+end
+
+puts liquid?(50)
