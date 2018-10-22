ad5e6618a4c2fca09644f42e8e2747b702891a89
[kou-sy] <e153416@eve.u-ryukyu.ac.jp>
2018-10-23 05:15:04 +0900

    fix typo

---
+++ ruby-book/lib/rgb.rb
@@ -0,0 +1,33 @@
+=begin
+def to_hex(r, g, b)
+  '#'+
+      r.to_s(16).rjust(2, '0')+
+      g.to_s(16).rjust(2, '0')+
+      b.to_s(16).rjust(2, '0')
+end
+=end
+
+=begin
+def to_hex(r, g, b)
+  hex = '#'
+  [r, g, b].each do |n|
+    hex += n.to_s(16).rjust(2, '0')
+end
+hex
+end
+=end
+
+def to_hex(r, g, b)
+  [r, g, b].inject('#') do |hex, n|
+    hex + n.to_s(16).rjust(2, '0')
+  end
+end
+
+def to_ints(hex)
+  r = hex[1..2]
+  g = hex[3..4]
+  b = hex[5..6]
+    [r, g, b].map do |s|
+    s.hex
+  end
+end
