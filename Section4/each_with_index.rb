a1b56028e643a9d50c718c3bfbc0abf99b93747a
[kou-sy] <e153416@eve.u-ryukyu.ac.jp>
2018-10-23 23:03:47 +0900

    ブロック引数を２つにする。リファクタリング No.3454

--- Section4/each_with_index.rb
+++ Section4/each_with_index.rb
@@ -4,3 +4,19 @@ fruits.each.with_index(1) { |fruit, i| puts "#{i}: #{fruit}"}
 
 #mapで処理しつつ、10から始まる添字を取得する
 fruits.map.with_index(10) { |fruit, i| puts "#{i}: #{fruit}"}
+
+dimensions = [
+    #[縦,横]
+    [10, 20],
+    [30, 40],
+    [50, 60],
+]
+#面積の計算結果を格納する配列
+areas = []
+#dimensions.each do |dimension|
+  dimensions.each do |length, width|
+#  length = dimension[0]
+#  width = dimension[1]
+  areas << length * width
+end
+puts areas
