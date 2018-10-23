5a7ec0d34b9c77b78a53cdaf9d1a3c2d82a820d6
[kou-sy] <e153416@eve.u-ryukyu.ac.jp>
2018-10-23 22:57:03 +0900

    添字付きの繰り返し処理 P.125

--- Section4/each_with_index.rb
+++ Section4/each_with_index.rb
@@ -1,2 +1,6 @@
 fruits = ['apple','orange','melon']
-fruits.each_with_index{|fruit, i| puts "#{i}:#{fruit}"}
+#each_with_indeではなく、each.with_indexにすると引数を渡せる
+fruits.each.with_index(1) { |fruit, i| puts "#{i}: #{fruit}"}
+
+#mapで処理しつつ、10から始まる添字を取得する
+fruits.map.with_index(10) { |fruit, i| puts "#{i}: #{fruit}"}
