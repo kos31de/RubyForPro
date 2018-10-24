46e5b4c9e3c5a2d219db507814d352175caee30d
[kou-sy] <e153416@eve.u-ryukyu.ac.jp>
2018-10-25 08:48:55 +0900

    for文の中で作成したローカル変数はfor文の外でも使える No.3647

---
+++ Section4/for.rb
@@ -0,0 +1,19 @@
+=begin
+numbers = [1, 2, 3, 4]
+sum = 0
+for n in numbers
+  sum += n
+end
+puts sum
+=end
+numbers = [1, 2, 3, 4]
+sum = 0
+#doを入れて1行で書く
+sum = 0
+for n in numbers do sum += n end
+puts sum
+#eachで書くのとほぼ同じ
+numbers.each do |n|
+  sum += n
+end
+puts sum
