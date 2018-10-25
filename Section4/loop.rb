f1b48c6a8483834c45cb967ea2c4ec7939133f53
[kou-sy] <e153416@eve.u-ryukyu.ac.jp>
2018-10-25 12:02:01 +0900

    whileだとループの外でもnが参照できる

--- Section4/loop.rb
+++ Section4/loop.rb
@@ -4,9 +4,17 @@ while true
 end
 =end
 
+=begin
 numbers = [1, 2, 3, 4, 5]
 loop do
   #sampleメソッドでランダムに要素を1つ取得する
+  n = numbers.sample
+  puts n
+  break if n == 5
+end
+=end
+numbers = [1, 2, 3, 4, 5]
+while true
   n = numbers.sample
   puts n
   break if n == 5
