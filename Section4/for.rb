970a7618076732f1f9304fd0b4f44d09f732d978
[kou-sy] <e153416@eve.u-ryukyu.ac.jp>
2018-10-25 11:51:00 +0900

    name errorではなくsyntax errorがでる No.3647

--- Section4/for.rb
+++ Section4/for.rb
@@ -6,12 +6,15 @@ for n in numbers
 end
 puts sum
 =end
+
 numbers = [1, 2, 3, 4]
 sum = 0
+
 #doを入れて1行で書く
 sum = 0
 for n in numbers do sum += n end
 puts sum
+
 #eachで書くのとほぼ同じ
 numbers.each do |n|
   sum += n
---
+++ Section4/for_2.rb
@@ -0,0 +1,18 @@
+numbers = [1, 2, 3, 4]
+sum = 0
+numbers.each do |n|
+  sum_value = n.even? ? n*10:n
+  sum += sum_value
+end
+
+puts n
+puts sum_value
+
+sum = 0
+for n in numbers
+  sum_value = n.even? n*10:n
+  sum += sum_value
+end
+
+puts n
+puts sum_value
