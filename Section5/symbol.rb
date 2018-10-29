5d42872382972ed1a79d7ba076fed1aa2a8c4f87
[kou-sy] <e153416@eve.u-ryukyu.ac.jp>
2018-10-30 00:51:45 +0900

    symbol:valueで書いても取り出す時は:symbol No.4023

---
+++ Section5/symbol.rb
@@ -0,0 +1,9 @@
+#文字列がキー
+currencies = {'japan' => 'yen', 'us' => 'dollar', 'india' => 'rupee'}
+puts currencies['japan']
+#symbolがキー
+currencies = {:japan => 'yen', :us => 'dollar', :india => 'rupee'}
+puts currencies[:japan]
+#シンボル:値で書いていい
+currencies = {japan:'yen', us:'dollar', india:'rupee'}
+puts currencies[:us]
