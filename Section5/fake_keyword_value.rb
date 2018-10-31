ae7d12961ff75e3f9f20ed73d05a8b926a140eaf
[kou-sy] <e153416@eve.u-ryukyu.ac.jp>
2018-10-31 19:11:50 +0900

    存在しないキーワードを渡すとエラーになる No.4283

--- Section5/fake_keyword_value.rb
+++ Section5/fake_keyword_value.rb
@@ -1,6 +1,7 @@
 #ハッシュを引数として受け取り、疑似キーワード引数を実現する
-def buy_burger(menu, options ={})
+def buy_burger(menu, drink: true, potato: true)
   puts drink = options[:drink]
   puts potato = options[:potato]
 end
-buy_burger('cheese', drink: true, potato: true)
+#saladとchickenは無効なキーワード引数なのでエラーになる
+buy_burger('fish', drink: true, potato: false, salad: true, chicken: false)
