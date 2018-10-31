057c6b95c4a9c9989328b91dbdd82b65628ca832
[kou-sy] <e153416@eve.u-ryukyu.ac.jp>
2018-10-31 19:02:23 +0900

    特別な要件がない限り、疑似ではなくキーワード引数を使うべき No.4272

---
+++ Section5/fake_keyword_value.rb
@@ -0,0 +1,6 @@
+#ハッシュを引数として受け取り、疑似キーワード引数を実現する
+def buy_burger(menu, options ={})
+  puts drink = options[:drink]
+  puts potato = options[:potato]
+end
+buy_burger('cheese', drink: true, potato: true)
