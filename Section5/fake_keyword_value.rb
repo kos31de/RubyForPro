48a53c8dc955fd2c65e94fefe1992711b8ccd539
[kou-sy] <e153416@eve.u-ryukyu.ac.jp>
2018-10-31 19:19:46 +0900

    ハッシュリテラルの{}がブロックだと解釈されてしまった No.4332

--- Section5/fake_keyword_value.rb
+++ Section5/fake_keyword_value.rb
@@ -5,3 +5,5 @@ def buy_burger(menu, drink: true, potato: true, **others)
 end
 #saladとchickenは無効なキーワード引数なのでエラーになる
 buy_burger('fish', drink: true, potato: false, salad: true, chicken: false)
+
+#最後の引数がハッシュのときのみハッシュリテラルの{}を省略できる
---
+++ Section5/hash_and_block.rb
@@ -0,0 +1,5 @@
+def buy_burger(options = {}, menu)
+  puts options
+end
+
+buy_burger{'drink' => true, 'potato' => false}, 'fish'
