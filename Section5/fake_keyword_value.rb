75225ce19fd3c54467291c9cdfc0107efb0b92a3
[kou-sy] <e153416@eve.u-ryukyu.ac.jp>
2018-10-31 19:14:05 +0900

    引数の最後に**othersをつけると、指定されていないキーワードがハッシュとして格納される No.4283

--- Section5/fake_keyword_value.rb
+++ Section5/fake_keyword_value.rb
@@ -1,7 +1,7 @@
 #ハッシュを引数として受け取り、疑似キーワード引数を実現する
-def buy_burger(menu, drink: true, potato: true)
-  puts drink = options[:drink]
-  puts potato = options[:potato]
+def buy_burger(menu, drink: true, potato: true, **others)
+
+  puts others
 end
 #saladとchickenは無効なキーワード引数なのでエラーになる
 buy_burger('fish', drink: true, potato: false, salad: true, chicken: false)
