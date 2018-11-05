f653efca0005ef18c0d63a7c00a94c849b34eab2
[kou-sy] <e153416@eve.u-ryukyu.ac.jp>
2018-11-06 08:48:02 +0900

    スペースがどこに必要? No.4907

---
+++ Section6/gsub_1.rb
@@ -0,0 +1,7 @@
+text = '誕生日は1977年7月17日です'
+puts text.gsub(/(\d+)年(\d+)月(\d+)日/, '\1-\2-\3')
+#名前付きキャプチャの場合は\k<name>のようにして参照できる
+puts text.gsub(
+    /(?<year>\d+)年(?<month>\d+)月(?<day>\d+)日/,
+    '\k<year>-\k<month>-\k<day>'
+)
