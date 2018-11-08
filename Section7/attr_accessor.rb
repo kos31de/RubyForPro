0426887af249db4c4f4bc9bcb27283d4eb9d4b68
[kou-sy] <e153416@eve.u-ryukyu.ac.jp>
2018-11-08 15:37:52 +0900

    リーダーではインスタンス変数変更できない No.5426

--- Section7/attr_accessor.rb
+++ Section7/attr_accessor.rb
@@ -1,6 +1,6 @@
 class User
   # @nameを読み書きするメソッドが自動的に表示される
-  attr_accessor :name
+  attr_reader :name
 
   def initialize(name)
     @name = name
@@ -9,7 +9,7 @@ class User
   # nameメソッドやname=メソッドを明示的に定義する必要がない
 end
 user = User.new('Alice')
-# @nameを変更する
+# @nameを変更しようとするとNoMethodErrorになる
 user.name = 'Bob'
 # @nameを参照する
-puts user.name
+puts user.names
