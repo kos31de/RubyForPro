e89c851496a7c0e5fcf3a12e22763dc323639047
[kou-sy] <e153416@eve.u-ryukyu.ac.jp>
2018-11-08 14:13:00 +0900

    インスタンス作成時にインスタンス変数に代入するのは同じ名前? No.5369

---
+++ Section7/instance_method__2.rb
@@ -0,0 +1,14 @@
+class User
+  def initialize(name)
+  #インスタンス作成時に渡された名前をインスタンス変数に保存する
+  @name = name
+  end
+
+
+  def hello
+    #インスタンス変数に保存されている名前を表示する
+    puts "Hello, I am #{@name}."
+  end
+end
+user = User.new('Alice')
+user.hello
