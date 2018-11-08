7234be295f18db14aa616a26b251f842857397cd
[kou-sy] <e153416@eve.u-ryukyu.ac.jp>
2018-11-08 18:32:47 +0900

    定数はインスタンスメソッドでもクラスメソッドでも同じ方法で参照できる No.5511

---
+++ Section7/const.rb
@@ -0,0 +1,14 @@
+class Product
+  # デフォルトの価格を定数として定義する。定数は大文字で始める
+  DEFAULT_PRICE = 0
+
+  attr_reader :name, :price
+  
+  def initialize(name, price = DEFAULT_PRICE)
+    @name = name
+    @price = price
+  end
+end
+
+product = Product.new('A free movie')
+puts product.price
