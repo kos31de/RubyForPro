6301d4f248b45ae9c86045404f02b55107e91b75
[kou-sy] <e153416@eve.u-ryukyu.ac.jp>
2018-11-09 00:21:18 +0900

    add enter&exit No.5621

--- ruby-book/test/gate_test.rb
+++ ruby-book/test/gate_test.rb
@@ -1,9 +1,14 @@
 require 'minitest/autorun'
 require '../lib/gate'
+require '../lib/ticket'
 
 class GateTest < Minitest::Test
   def test_gate
-    # とりあえずGateオブジェクトが作れることを確認する
-    assert Gate.new
+    umeda = Gate.new(:umeda)
+    juso = Gate.new(:juso)
+
+    ticket = Ticket.new(150)
+    umeda.enter(ticket)
+    assert juso.exit(ticket)
   end
 end
