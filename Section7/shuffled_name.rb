249905b2d062cae143200b37811f90f5f4834ddc
[kou-sy] <e153416@eve.u-ryukyu.ac.jp>
2018-11-08 14:39:43 +0900

    initializeメソッドを追加 No.5369

--- Section7/shuffled_name.rb
+++ Section7/shuffled_name.rb
@@ -1,9 +1,12 @@
 class User
-#省略
+  def initialize(name)
+    @name = name
+  end
+
   def hello
     # shuffled_nameはローカル変数
     shuffled_name = @name.chars.shuffle.join
-    puts "Hello, I am #{shuffled_name}"
+    puts "Hello, I am #{shuffled_name}."
   end
 end
 user = User.new('Alice')
