6ba13d3167f316597e3c95576d19d0a1811d6104
[kou-sy] <e153416@eve.u-ryukyu.ac.jp>
2018-11-08 14:44:10 +0900

    インスタンス変数は代入する前に参照してもエラーにならない

--- Section7/shuffled_name.rb
+++ Section7/shuffled_name.rb
@@ -1,12 +1,13 @@
 class User
   def initialize(name)
-    @name = name
+    #わざとコメントアウト
+    #@name = name
   end
 
   def hello
     # shuffled_nameはローカル変数
-    #shuffled_name = @name.chars.shuffle.join
-    puts "Hello, I am #{shuffled_name}."
+    
+    puts "Hello, I am #{@name}."
   end
 end
 user = User.new('Alice')
