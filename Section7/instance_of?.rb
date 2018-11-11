7c8eacbed2e7e04de73923d1134de4519f3a46ff
kou_sy <e153416@eve.u-ryukyu.ac.jp>
2018-11-11 19:05:54 +0900

    is_a?で継承関係を確認

--- Section7/instance_of?.rb
+++ Section7/instance_of?.rb
@@ -3,7 +3,12 @@ end
 
 user = User.new
 
-# userはUserクラスのインスタンスか?
-puts user.instance_of?(User)
-# userはStringクラスのインスタンスか?
-puts user.instance_of?(String)
+# instance_of?はクラスが全く同じでないとtrueにならない
+puts user.instance_of?(Object)
+# is_a?はis-a関係にあればtrueになる
+puts user.is_a?(User)
+puts user.is_a?(Object)
+puts user.is_a?(BasicObject)
+
+# is-aの関係にない場合はfalse
+user.is_a?(String)
