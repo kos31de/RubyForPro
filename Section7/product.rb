94a849228a5282f6309f44db2d766adfee832e95
kou_sy <e153416@eve.u-ryukyu.ac.jp>
2018-11-11 21:07:31 +0900

    to_sメソッドは呼び出せた No.6008

--- Section7/product.rb
+++ Section7/product.rb
@@ -5,23 +5,19 @@ class Product
     @name = name
     @price = price
   end
+end
 
-  # 金額を整形するクラスメソッド
-  def self.format_price(price)
-    "#{price}円"
-  end
+class DVD < Product
+  attr_reader :running_time
 
-  def to_s
-    # インスタンスメソッドからクラスメソッドを呼び出す
-    formatted_price = Product.format_price(price)
-    "name: #{name}, price: #{formatted_price}"
+  def initialize(name, price, running_time)
+    super(name, price)
+    @running_time = running_time
   end
 end
 
 product = Product.new('A great movie', 1000)
-product.to_s
+puts product.to_s
 
-# クラス名.メソッドの形式でクラスメソッドを呼び出す
-Product.format_price(price)
-# self.class.メソッドの形式でクラスメソッドを呼び出す
-self.class.format_price(price)
+dvd = DVD.new('An awesome film', 3000, 120)
+puts dvd.to_s
