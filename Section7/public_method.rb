64b357099c9c100ef87d0c58bdb780293851fac4
kou_sy <e153416@eve.u-ryukyu.ac.jp>
2018-11-11 21:27:45 +0900

    呼び出せてないけど、この順ではエラーにならないのでは? No.6066

--- Section7/public_method.rb
+++ Section7/public_method.rb
@@ -1,11 +1,14 @@
 class User
-  # privateはレシーバを指定して呼び出せない
-  private
   def hello
-    puts 'Hello!'
+    # nameメソッドはprivateなのでselfをつけるとエラーになる
+    puts 'Hello! I am #{self.name}.'
+  end
+
+  private
+
+  def name
+    puts 'Alice'
   end
 end
 user = User.new
-# publicメソッドなのでクラスの外部から呼び出せる
 user.hello
-
