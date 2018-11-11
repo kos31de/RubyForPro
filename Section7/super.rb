a610720f610977e287ac0adb921e0de63b270a8a
kou_sy <e153416@eve.u-ryukyu.ac.jp>
2018-11-11 20:59:06 +0900

    引数の数は違ってエラーになってよい

--- Section7/super.rb
+++ Section7/super.rb
@@ -2,19 +2,19 @@ require_relative './product_class.rb'
 
 class  DVD < Product
   # nameとpriceはスーパークラスでattr_readerが設定されているので定義不要
-  attr_reader :running_time
-
-  def initialize(name, price, running_time)
+  #スーパークラスに処理を任せる
+  #def initialize(name, price, running_time)
     # 引数をスーパークラスのメソッドにわたす。つまりsuper(name, price)
     # super()は引数無しでスーパークラスのメソッドを呼び出す
-    super()
-    @running_time = running_time
-  end
+  #  super()
+  #  @running_time = running_time
+  #end
 end
 
-dvd = DVD.new('A great movie', 1000, 120)
+#DVDクラスをnewすると、自動的にsuper classのinitializeメソッドが呼ばれる
+dvd = DVD.new('A great movie', 1000)
 puts dvd.name
 puts dvd.price
-puts dvd.running_time
+
 
 # 実行してみるとこの下にproduct_class.rbの出力がある
