b4e94c878d004b13c8bd1727c50d30052a63de81
kou_sy <e153416@eve.u-ryukyu.ac.jp>
2018-11-13 09:53:41 +0900

    インスタンス変数はクラスをインスタンス化した際に、オブジェクトごとに管理される。クラスインスタンス変数はインスタンスの作成とは無関係にクラス自身が保持しているデータ No.6327

---
+++ Section7/class_instance.rb
@@ -0,0 +1,26 @@
+class Product
+  # クラスインスタンス変数
+  @name = 'Product'
+
+  def self.name
+    # クラスインスタンス変数
+    @name
+  end
+
+  def initialize(name)
+    # インスタンス変数
+    @name = name
+  end
+
+  # attr_reader :nameでもいいが、@nameの中身を意識するためにあえてメソッドを定義する
+  def name
+    # インスタンス変数
+    @name
+  end
+end
+
+Product.name
+
+puts product = Product.new('A great movie')
+puts product.name
+puts Product.name
