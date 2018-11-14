0e07a69dab478c81e5da57c4b38814b4477d80b6
kou-sy <e153416@eve.u-ryukyu.ac.jp>
2018-11-14 18:20:19 +0900

    Rubyで便宜上クラスメソッドと呼んでいるものは、実際は特定のクラスの特異メソッド No.6659

---
+++ Section7/singular_class.rb
@@ -0,0 +1,26 @@
+# クラスメソッドを定義するコード例
+class User
+  def self.hello
+    puts 'Hello.'
+  end
+
+  class << self
+    def hi
+    puts 'Hi.'
+    end
+  end
+end
+
+# 特異メソッドを定義するコード例
+alice = 'I am alice.'
+
+def alice.hello
+  puts 'Hello.'
+end
+
+class << alice
+  def hi
+    puts 'Hi.'
+  end
+end
+
