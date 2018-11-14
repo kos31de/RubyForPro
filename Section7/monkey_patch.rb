e9bbfe1898d4efb54e40e7b527d7c89636877f60
kou-sy <e153416@eve.u-ryukyu.ac.jp>
2018-11-14 16:50:09 +0900

    クラスを上書きするのがモンキーパッチ No.6595

---
+++ Section7/monkey_patch.rb
@@ -0,0 +1,24 @@
+# 以下のUserクラスは外部ライブラリで定義されている想定
+class User
+  def initialize(name)
+    @name = name
+  end
+
+  def hello
+    puts "Hello, #{@name}!"
+  end
+end
+
+# モンキーパッチを当てる前の挙動
+user = User.new('Alice')
+user.hello
+
+# helloメソッドにモンキーパッチをあてて独自の挙動をもたせる
+class User
+  def hello
+    puts "#{@name}さん、こんにちは!"
+  end
+end
+
+# メソッドの定義を上書きしたのでhelloメソッドの挙動が変わった
+user.hello
