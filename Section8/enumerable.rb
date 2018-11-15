6e9e2cca65ca2e12aa6ef82ff1b0b1b1ef9295a3
kou-sy <e153416@eve.u-ryukyu.ac.jp>
2018-11-16 07:59:18 +0900

    {a: 1}の書き方でSyntax Eroor No.7148

---
+++ Section8/enumerable.rb
@@ -0,0 +1,14 @@
+# Enumerableがincludeされているモジュール
+puts Array.include?(Enumerable)
+puts Hash.include?(Enumerable)
+puts Range.include?(Enumerable)
+
+# 配列、ハッシュ、範囲でmapメソッドを使う
+puts [1, 2, 3].map {|n| n * 10}
+#puts {a:1, b:2, c:3}.map {|k, v| [k, v * 10]}
+puts {1..3}.map {|n| n * 10}
+
+# 配列、ハッシュ、範囲でcountメソッドを使う
+puts [1, 2, 3].count
+#puts {a: 1, b: 2, c: 3}.count
+puts {1..3}.count
