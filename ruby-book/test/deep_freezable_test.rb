cda0241f861b9d171a02fbb24e7d45326b10c73e
kou-sy <e153416@eve.u-ryukyu.ac.jp>
2018-11-15 09:39:24 +0900

    とりあえずdeep_freezeモジュール完成

--- ruby-book/test/deep_freezable_test.rb
+++ ruby-book/test/deep_freezable_test.rb
@@ -13,4 +13,16 @@ class DeepFreezableTest < Minitest::Test
     # 配列のすべての要素がfreezeされているか?
     assert Team::COUNTRIES.all? {|country| country.frozen?}
   end
+
+  def test_deep_freeze_to_hash
+    # ハッシュの値は正しいか?
+    assert_equal(
+      {'Japan' => 'yen', 'US' => 'dollar', 'India' => 'rupee'},
+      Bank::CURRENCIES
+    )
+    # ハッシュ自身がfreezeされているか?
+    assert Bank::CURRENCIES.frozen?
+    # ハッシュの要素(キーと値)が全てfreezeされているか?
+    assert Bank::CURRENCIES.all? {|key, value| key.frozen? &&value.frozen?}
+  end
 end
