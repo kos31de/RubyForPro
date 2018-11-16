2053a521367c1e5cf1ccc613a09bfbd44fd6f828
kou-sy <e153416@eve.u-ryukyu.ac.jp>
2018-11-16 19:46:14 +0900

    syntax error, unexpected :, expecting } No.7148

--- Section8/enumerable.rb
+++ Section8/enumerable.rb
@@ -5,10 +5,10 @@ puts Range.include?(Enumerable)
 
 # 配列、ハッシュ、範囲でmapメソッドを使う
 puts [1, 2, 3].map {|n| n * 10}
-#puts {a:1, b:2, c:3}.map {|k, v| [k, v * 10]}
-puts {1..3}.map {|n| n * 10}
+puts { a: 1, b: 2, c: 3 }.map { |k, v| [k, v * 10] }
+puts (1..3).map { |n| n * 10 }
 
 # 配列、ハッシュ、範囲でcountメソッドを使う
 puts [1, 2, 3].count
-#puts {a: 1, b: 2, c: 3}.count
-puts {1..3}.count
+puts {a: 1, b: 2, c: 3}.count
+puts (1..3).count
