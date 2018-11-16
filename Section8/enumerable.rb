a913a6b02bc78d91364aa22f455ed0c88b8f4507
kou-sy <e153416@eve.u-ryukyu.ac.jp>
2018-11-16 19:54:32 +0900

    pにしてnilも出力 No.7167

--- Section8/enumerable.rb
+++ Section8/enumerable.rb
@@ -5,6 +5,7 @@ puts Range.include?(Enumerable)
 
 # 配列、ハッシュ、範囲でmapメソッドを使う
 puts [1, 2, 3].map {|n| n * 10}
+# a: 1がハッシュにならない???
 puts { a: 1, b: 2, c: 3 }.map { |k, v| [k, v * 10] }
 puts (1..3).map { |n| n * 10 }
 
---
+++ Section8/ufo.rb
@@ -0,0 +1,9 @@
+puts 2 <=> 1
+puts 2 <=> 2
+puts 1 <=> 2
+p 2 <=> 'abc'
+
+puts 'xyz' <=> 'abc'
+puts 'abc' <=> 'abc'
+p 'abc' <=> 'xyz'
+
