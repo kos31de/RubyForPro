c1163afe44de6a969f31cf4899d0f0d71e6fbc4f
kou-sy <e153416@eve.u-ryukyu.ac.jp>
2018-11-17 22:20:38 +0900

    Mathモジュール

---
+++ Section8/math_module.rb
@@ -0,0 +1,17 @@
+# モジュールの特異メソッドとしてsqrt(平方根)メソッドを利用する
+Math.sqrt(2)
+
+class Calculator
+  include Math
+
+  def calc_sqrt(n)
+    # ミックスインとしてMathモジュールのsqrtメソッドを使う
+    puts sqrt(n)
+  end
+end
+
+calculator = Calculator.new
+calculator.calc_sqrt(2)
+
+puts Math::E
+puts Math::PI
