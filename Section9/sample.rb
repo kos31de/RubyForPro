0e1f6323cf0be65281d7dc5aca1459ff712e71b1
kou-sy <e153416@eve.u-ryukyu.ac.jp>
2018-11-17 23:14:43 +0900

    begin, rescue, endで例外処理 No.7832

--- Section9/sample.rb
+++ Section9/sample.rb
@@ -4,7 +4,13 @@ module Greeter
     puts 'hello'
   end 
 end
+
+# 例外処理を組み込んで例外に対処する
+begin
 greeter = Greeter.new
-# 上の行で例外が発生するため、ここから下は実行されない
+# 上の行で例外が発生する(モジュールからインスタンスは作れない)ため、ここから下は実行されない
+rescue
+  puts '例外が発生したが、このまま続行する'
+end
 puts 'End.'
 
