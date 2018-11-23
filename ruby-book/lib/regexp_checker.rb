8ee222c88344ebe35573d7fa1469d3bc36c6e880
kou-sy <e153416@eve.u-ryukyu.ac.jp>
2018-11-23 17:20:25 +0900

    例外処理はうまくいってるけど、エラーメッセージが出ていない No.8235

--- ruby-book/lib/regexp_checker.rb
+++ ruby-book/lib/regexp_checker.rb
@@ -4,7 +4,16 @@ text = gets.chomp
 print 'Pattern?:'
 pattern = gets.chomp
 
-regexp = Regexp.new(pattern)
+# 例外処理を組み込んで再入力可能にする
+begin
+  print 'Pattern?:'
+  pattern = gets.chomp
+  regexp = Regexp.new(pattern)
+rescue RegexpError
+  puts "Invalid pattern: #{e.message}"
+  retry
+end
+
 matches = text.scan(regexp)
 if matches.size > 0
   puts "Matched: #{matches.join(',')}"
