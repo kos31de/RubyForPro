22326daa866765f9adb466f7896e6314f0a9ef1a
kou-sy <e153416@eve.u-ryukyu.ac.jp>
2018-11-23 17:26:29 +0900

    rescureはなくてもいい。 No.8260

---
+++ Section9/else.rb
@@ -0,0 +1,16 @@
+# elseに入ったら例外を発生させるパターン(良い例)
+def currency_of(country)
+  case country
+  when :japan
+    'yen'
+  when :us
+    'dollar'
+  when :india
+    'rupee'
+  else
+    raise ArgumentError, "無効な国名です。#{country}"
+  end
+end
+# 例外が発生する
+currency_of(:italy)
+
---
+++ Section9/ensure.rb
@@ -0,0 +1,11 @@
+# 書き込みモードでファイルを開く
+file = File.open('some.txt', 'w')
+
+begin
+  # ファイルに文字列を書き込む
+  file << 'Hello'
+ensure
+  # 例外の有無にかかわらず必ずファイルをクロースする
+  file.close
+end
+
