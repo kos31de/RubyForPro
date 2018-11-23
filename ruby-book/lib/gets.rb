bb8775b670b47914878a7c6cb673898bbf322d6e
kou-sy <e153416@eve.u-ryukyu.ac.jp>
2018-11-23 17:04:26 +0900

    標準入出力 No.8194

---
+++ ruby-book/lib/gets.rb
@@ -0,0 +1,8 @@
+# getsメソッドを呼ぶとプログラムはユーザーの入力を待つ
+input = gets
+
+input = input.chomp
+# chompメソッドを使って改行文字を削除
+input
+
+# 実際にはinput = gets.chompだけで良い
---
+++ ruby-book/lib/regexp_checker.rb
@@ -0,0 +1,5 @@
+# printは文字列を表示した後に改行しない
+print 'Text?:'
+text = gets.chomp
+# 動作確認のため変数の中身を表示
+puts text
