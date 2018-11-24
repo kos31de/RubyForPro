d464a64eac78b78e734e9e87766be93506d75fcb
kou-sy <e153416@eve.u-ryukyu.ac.jp>
2018-11-24 23:28:18 +0900

    メソッド全体が例外処理の対象になっている時 No.8349

---
+++ Section9/fizz_buzz.rb
@@ -0,0 +1,17 @@
+def fizz_buzz(n)
+begin 
+  if n % 15 == 0
+    puts 'Fizz Buzz'
+  elsif n % 3 == 0
+    puts 'Fizz'
+  elsif n % 5 == 0
+    puts 'Buzz'
+  else
+    n.to_s
+  end
+rescue => e
+      puts "#{e.class} #{e.message}"
+  end
+end
+
+fizz_buzz(nil)
