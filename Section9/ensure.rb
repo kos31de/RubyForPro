557b812de8f238990e8c7cfcd1983a956a7b3c91
kou-sy <e153416@eve.u-ryukyu.ac.jp>
2018-11-24 16:24:11 +0900

    例外は発生するけど、openメソッドによってクローズ処理自体は必ず行われる No.8273

--- Section9/ensure.rb
+++ Section9/ensure.rb
@@ -1,4 +1,13 @@
 # ブロック付きでオープンすると、メソッドの実行後に自動的にクローズされる
 File.open('some.txt', 'w') do |file|
   file << 'Hello0000'
+  # わざと例外を発生
+  1 / 0
 end
+=begin
+❯ ruby ensure.rb
+ensure.rb:5:in `/': divided by 0 (ZeroDivisionError)
+        from ensure.rb:5:in `block in <main>'
+        from ensure.rb:2:in `open'
+        from ensure.rb:2:in `<main>'
+=end
