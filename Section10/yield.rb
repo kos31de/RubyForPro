b8d083a554ba83f51cf004a120b9f27dd51bcd97
kou-sy <e153416@eve.u-ryukyu.ac.jp>
2018-11-28 11:26:56 +0900

    yieldは引数の過不足に寛容 No.8550

--- Section10/yield.rb
+++ Section10/yield.rb
@@ -1,13 +1,25 @@
 def greeting
   puts 'おはよう'
-  # ここでブロックの処理を呼び出す
-  if block_given?
-  yield
-  end
+  # ブロックに引数を渡し、戻り値を受け取る
+  text = yield 'こんにちは'
+  # ブロックの戻り値をコンソールに出力する
+  puts text
   puts 'こんばんは'
 end
 
-greeting do
-  puts 'こんにちは'
+greeting do |text|
+  # ブロック引数が1つであれば、2つ目の引数は無視される
+  text * 2
 end
 
+def greeting
+  # 1この引数をブロックに渡す
+text = yield 'こんにちは'
+puts text
+puts 'こんにちは'
+end
+
+greeting do |text, other|
+  # ブロック引数が2つであれば、2つめの引数はnilになる
+  text * 2 + other.inspect
+end
