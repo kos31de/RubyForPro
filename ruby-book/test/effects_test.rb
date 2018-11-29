58c7435f44dbc79ec75752de6d93c6a14ee8ab05
kou-sy <e153416@eve.u-ryukyu.ac.jp>
2018-11-29 13:49:31 +0900

    test_echoを追加、テスト盛大に落ちる No.8855

--- ruby-book/test/effects_test.rb
+++ ruby-book/test/effects_test.rb
@@ -4,7 +4,15 @@ require_relative '../lib/effects'
 class EffectTest < Minitest::Test
   def test_reverse
     effect = Effects.reverse
-    assert_equal 'ybuR si !nuf', effect.call("Ruby is fun!")
+    assert_equal 'ybuR si !nuf', effect.call('Ruby is fun!')
+  end
+  def test_echo
+    effect = Effects.echo(2)
+    assert_equal 'RRuubbyy iiss ffuunn!!', effect.call('Ruby is fun!')
+
+    effect = Effects.echo(3)
+    assert_equal 'RRRuuubbbyyy iiisss fffuuunnn!!!', effect.call('Ruby is fun!')
   end
 end
+
 
