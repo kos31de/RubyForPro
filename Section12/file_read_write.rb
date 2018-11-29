f1fa2a5985315302983bf53b5473f6c94c89a99e
kou-sy <e153416@eve.u-ryukyu.ac.jp>
2018-11-29 19:15:51 +0900

    ../にした

--- Section12/file_read_write.rb
+++ Section12/file_read_write.rb
@@ -1,7 +1,7 @@
-File.open('./lib/fizz_buzz.rb', 'r') do |f|
+File.open('../lib/fizz_buzz.rb', 'r') do |f|
   puts f.readlines.count
 end
 
-File.open('./lib/hello_world.txt', 'w') do |f|
+File.open('../lib/hello_world.txt', 'w') do |f|
   f.puts 'Hello, world!'
 end
