3d5c5c3888927688b9ca57cb1c8bb4d273ab9528
kou-sy <e153416@eve.u-ryukyu.ac.jp>
2018-11-29 19:18:32 +0900

    libディレクトリだと実行できた。相対パスに問題あるっぽい。 No.9594

--- Section12/file_read_write.rb
+++ Section12/file_read_write.rb
@@ -1,7 +1,8 @@
+#lib内のfizz_buzz.rbの行数をターミナルに表示
 File.open('../lib/fizz_buzz.rb', 'r') do |f|
   puts f.readlines.count
 end
-
+# libにhello_world.txtを作成して文章を書き込む
 File.open('../lib/hello_world.txt', 'w') do |f|
   f.puts 'Hello, world!'
 end
