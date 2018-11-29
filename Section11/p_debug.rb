a2d131f526e17281e43ec8f06a54fa52f0dd54de
kou-sy <e153416@eve.u-ryukyu.ac.jp>
2018-11-29 18:11:53 +0900

    tapメソッドはブロック引数にレシーバをそのまま渡す。ブロックの戻り値は無視され、tapメソッド全体の戻り値はレシーバ自身になる。 No.9223

---
+++ Section11/p_debug.rb
@@ -0,0 +1,8 @@
+def calc_fare(ticket)
+  from = STATIONS.index(ticket.stamped_at)
+  to = STATIONS.index(@name)
+  # to - fromの結果をターミナルに出力しつつ、変数distanceに代入する
+  distance = p to - from
+  FARES[distance - 1]
+end
+calc_fare()
