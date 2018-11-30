bf8b9cccc259ecabab7b4171562e875653b4fa94
kou-sy <e153416@eve.u-ryukyu.ac.jp>
2018-11-30 14:57:04 +0900

    また相対パス読み込めてない? No.9830

--- ruby-book/test/fizz_buzz_test.rb
+++ ruby-book/test/fizz_buzz_test.rb
@@ -3,8 +3,6 @@ require_relative '../lib/fizz_buzz'
 
 class FizzBuzzTest < Minitest::Test
   def test_fizz_buzz
-    require 'byebug'
-    byebug
     assert_equal'1', fizz_buzz(1)
     assert_equal'2', fizz_buzz(2)
     assert_equal'Fizz', fizz_buzz(3)
