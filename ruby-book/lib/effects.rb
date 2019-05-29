e967af90bda3bff8722509cfe99cad813ccceb35
Kosei Ide <koseiide@gmail.com>
2019-05-29 13:14:05 +0900

    joinとsplitの引数の引用符の中にスペースがなかっただけだった。

--- ruby-book/lib/effects.rb
+++ ruby-book/lib/effects.rb
@@ -1,7 +1,7 @@
 module Effects
   def self.reverse
     ->(words) do
-      words.split('').map(&:reverse).join('')
+      words.split(' ').map(&:reverse).join(' ')
     end
   end
 
@@ -9,14 +9,14 @@ module Effects
     ->(words) do
       # スペースならそのまま返す
       # スペース以外ならその文字を指定された回数だけ繰り返す
-      words.chars.map {|c| c == '' ? c:c * rate}.join
+      words.chars.map {|c| c == ' ' ? c:c * rate}.join
     end
   end
 
   def self.loud(level)
     ->(words) do
       # スペースで分割 > 大文字変換と"!"の付与 > スペースで連結
-      words.split('').map { |word| word.upcase + '!' * level}.join('')
+      words.split(' ').map { |word| word.upcase + '!' * level}.join(' ')
     end
   end
 end
--- ruby-book/lib/word_synth.rb
+++ ruby-book/lib/word_synth.rb
@@ -1,3 +1,4 @@
+# require_relative '../lib/effects'
 class WordSynth
   def initialize
     @effects = []
